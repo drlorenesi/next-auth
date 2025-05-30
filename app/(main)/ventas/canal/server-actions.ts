c48c678de468a4733f3a615eff660e9eaa5ab68d
Diego Lorenesi <drlorenesi@gmail.com>
2025-05-30 11:05:28 -0600

    Refactor handleDates function to use FormData type

--- app/(main)/ventas/canal/server-actions.ts
+++ app/(main)/ventas/canal/server-actions.ts
@@ -1,8 +1,8 @@
 "use server";
 
-import { type DateFormData } from "./form-schema";
+import { type FormData } from "./form-schema";
 
-export async function handleDates(formData: DateFormData) {
+export async function handleDates(formData: FormData) {
   console.log("Received form data:", formData);
 
   return { success: true, data: formData };
